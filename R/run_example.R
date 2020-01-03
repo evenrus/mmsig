bac6d696bca9772f3e5a1aaeefc43092f76b95ff
evenrus <evenholth@gmail.com>
2020-01-03 14:42:54 -0500

    updated example

--- R/run_example.R
+++ R/run_example.R
@@ -1,6 +1,6 @@
 # Example of running mmsig.
 # Fit previously defined reference mutational signatures to example data from a previously published cohort of multiple myeloma whole genomes
-# v 10.15.2019
+# v 01.03.2020
 
 # Import data
 # mmsig can take input data both as a matrix of 96 mutational classes profiles, and in a 5-column format where each row is a mutation with chromosome, position, etc. 
@@ -8,10 +8,7 @@
 # the current version of mmsig only supports 5-column format data aligned to hg19/GRCh37
 
 muts_example <- read.delim("../data/example_muts_96.txt", stringsAsFactors = F, header=T, row.names = 1) 
-
-muts_example_5cols <- read.delim("../data/example_muts_5cols.txt", stringsAsFactors = F, header=F) 
-names(muts_example_5cols) <- c("sample", "chr", "pos", "ref", "alt")
-muts_example_5cols$chr <- paste0("chr", muts_example_5cols$chr)
+muts_example_5cols <- read.delim("../data/example_muts_5cols.txt", stringsAsFactors = F, header=T) 
 
 # Import signature reference
 sig_ref <- read.delim("../data/mm_signature_definitions.txt", stringsAsFactors = F, header=T) 
@@ -33,10 +30,9 @@ sig_out <- mm_fit_signatures(muts.input=muts_example_5cols,
                              sample.sigt.profs = NULL, 
                              strandbias = TRUE,
                              bootstrap = TRUE,
-                             iterations = 20,
+                             iterations = 20, # 1000 iterations recommended for stable results
                              refcheck=TRUE,
                              dbg=FALSE) 
-
 
 # data summaries and plots
 plot_signatures(sig_out$estimate)
